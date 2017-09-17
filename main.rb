940b0a93769d8493b5e9350c264760dc63070c42
tim <tim.w@axsoftware.net>
2017-09-17 13:23:48 +0800

    add reload inject ui

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ require_relative 'mogg/web_client'
 require_relative 'mogg/bot/behavior'
 require_relative 'mogg/bot'
 
-RAW_INSERT_SCRIPT = "MOGG_TRIGGER = false; mogg_body = document.getElementsByTagName('body')[0];mogg_button = document.createElement('button');mogg_button.innerHTML = 'Start/Stop';mogg_button.style.cssText = 'position: fixed;display: block;left: 2%;top: 45%;z-index:1000;';mogg_button.onclick = function(){MOGG_TRIGGER = !MOGG_TRIGGER};mogg_body.insertBefore(mogg_button,mogg_body.firstChild);"
+RAW_INSERT_SCRIPT = "MOGG_TRIGGER = false; MOGG_RELOAD = false; mogg_body = document.getElementsByTagName('body')[0]; mogg_button = document.createElement('button');mogg_button.innerHTML = 'Start/Stop';mogg_button.style.cssText = 'position: fixed;display: block;left: 2%;top: 45%;z-index:1000;';mogg_button.onclick = function(){MOGG_TRIGGER = !MOGG_TRIGGER};mogg_body.insertBefore(mogg_button,mogg_body.firstChild);mogg_reload_button = document.createElement('button');mogg_reload_button.innerHTML = 'Reload';mogg_reload_button.style.cssText = 'position: fixed;display: block;left: 2%;top: 35%;z-index:1000;';mogg_reload_button.onclick = function(){MOGG_RELOAD = !MOGG_RELOAD};mogg_body.insertBefore(mogg_reload_button,mogg_body.firstChild);"
 
 web_client = WebClient.new url: SECRET_MOGG_URL, insert_script: RAW_INSERT_SCRIPT
 bot = Bot.new client: web_client, path: MOVE_PATH
